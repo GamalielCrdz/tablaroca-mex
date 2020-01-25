e69cbc633b71d96b1b960a9ec706388a5db6a241
GamalielCrdz <gamahappy98@gmail.com>
2020-01-24 18:54:34 -0600

    ahora funciona el navbar
    Signed-off-by: GamalielCrdz <gamahappy98@gmail.com>

--- js/navbar.js
+++ js/navbar.js
@@ -1,23 +1,11 @@
 const navbar = document.getElementById("navbar-container");
-const collapse = document.getElementById("bs-example-navbar-collapse-1");
-const links = document.getElementById("contact-links");
-const navbarTop = document.getElementById("contact-number");
-const logo = document.getElementById("logo");
 
 window.onscroll =  function(e) {
   const scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
   if (scroll >= 150) {
     navbar.classList.add('nav-colored');
-/*     links.style = 'display: none;'
-    collapse.style = 'display: none!important;';
-    logo.style = 'display: none;'
-    navbarTop.style = 'text-align: center; margin-top: 15px;' */
   } else {
-/*     logo.style = '';
-    navbarTop.classList.remove('navbar-top-scroll');
-    links.style = '';
-    collapse.style = ''; */
     navbar.classList.remove('nav-colored');
   }
 };
